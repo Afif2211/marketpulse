291a7cb25aeb34108d067c39e9d0d726198d5c38
Afif Ahmad <afifbalouch@gmail.com>
2026-08-02 22:40:01 +0100

    Capture auth token synchronously before React renders to fix race condition

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -7,6 +7,27 @@ import { AuthProvider } from "./context/AuthContext";
 import { PortfolioProvider } from "./context/PortfolioContext";
 import { MarketDataProvider } from "./context/MarketDataContext";
 
+const params = new URLSearchParams(window.location.search);
+
+const tokenFromUrl = params.get("token");
+
+if (tokenFromUrl) {
+
+    localStorage.setItem("token", tokenFromUrl);
+
+    params.delete("token");
+
+    const cleanedSearch = params.toString();
+
+    const newUrl =
+        window.location.pathname +
+        (cleanedSearch ? `?${cleanedSearch}` : "") +
+        window.location.hash;
+
+    window.history.replaceState({}, "", newUrl);
+
+}
+
 const root = ReactDOM.createRoot(
     document.getElementById("root")
 );
